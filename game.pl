695484dee27dd865d3c95ef52c338813c38b17f1
Martim Moniz <eloy.mtm@gmail.com>
2025-01-02 14:32:23 +0000

    Working game

--- game.pl
+++ game.pl
@@ -1,88 +1,241 @@
 :- use_module(library(lists)).
-:- use_module(library(random)).
+
+% Game configuration
+board_size(9, 5).  % 9x5 board
 
 % Game loop
 play :-
     initial_board(Board),
     display_game(Board),
-    play_game(Board).
+    play_game(Board, w).  % w moves first
 
-% Main game loop
-play_game(Board) :-
-    game_over(Board),
-    !,
-    announce_winner(Board).
-play_game(Board) :-
-    get_move(Move),
-    make_move(Board, Move, NewBoard),
-    display_game(NewBoard),
-    play_game(NewBoard).
-
-% Initial board setup (example 6x6)
-initial_board([
-    [1,2,1,2,1,2],
-    [2,1,2,1,2,1],
-    [1,2,1,2,1,2],
-    [2,1,2,1,2,1],
-    [1,2,1,2,1,2],
-    [2,1,2,1,2,1]
-]).
+% Initial board setup
+initial_board([[b,w,b,w,b,w,b,w,b],[w,e,e,e,e,e,e,e,w],[e,e,e,e,e,e,e,e,e],[b,e,e,e,e,e,e,e,b],[w,b,w,b,w,b,w,b,w]]).
 
 % Display the game board
 display_game(Board) :-
     nl,
-    display_board(Board),
+    write('   1   2   3   4   5   6   7   8   9'), nl,
+    display_board(Board, 1),
     nl.
 
 % Display board helper
-display_board([]).
-display_board([Row|Rest]) :-
-    write('|'),
-    display_row(Row),
+display_board([], _).
+display_board([Row|Rest], RowNum) :-
+    format('~w ', [RowNum]),
+    display_row(Row),  % Added closing border
     nl,
-    display_board(Rest).
+    write(' '), nl,
+    NextRow is RowNum + 1,
+    display_board(Rest, NextRow).
+
 
 display_row([]).
 display_row([Cell|Rest]) :-
+    display_cell(Cell),
     write(' '),
-    write(Cell),
-    write(' |'),
     display_row(Rest).
 
-% Get player move
-get_move((Row, Col)) :-
-    write('Enter row: '),
-    read(Row),
-    write('Enter column: '),
-    read(Col).
+display_cell(b) :- write('\e[48;5;0m B \e[0m').  % b piece with b background
+display_cell(w) :- write('\e[48;5;255m W \e[0m').  % w piece with w background
+display_cell(e) :- write('\e[48;5;244m   \e[0m').
 
-% Game over check (placeholder)
-game_over(Board) :-
-    % Add game over conditions here
-    false.
+% Get valid move
+get_valid_move(Board, Player, (FromRow, FromCol), (ToRow, ToCol), Final) :-
+    repeat,
+    write('Enter source position (row col): '),
+    read((FromRow, FromCol)),
+    (valid_position(FromRow, FromCol) -> 
+        write('Enter destination position (row col): '),
+        read((ToRow, ToCol)),
+        (valid_move(Board, Player, (FromRow, FromCol), (ToRow, ToCol), Final) -> 
+            true
+        ;   
+            write('Invalid move, try again.'), nl,
+            fail
+        )
+    ;   
+        write('Invalid position, try again.'), nl,
+        fail
+    ),
+    other_player(Player, OtherPlayer).
 
-% Make move (placeholder)
-make_move(Board, (Row, Col), NewBoard) :-
-    % Add move logic here
-    NewBoard = Board.
+% Valid move check
+valid_move(Board, Player, From, To ,Final) :-
+        piece_at(Board, From, Player),
+        
+        calculate_dir(From, To, Dir),
+        write('Direction calculated: '), write(Dir), nl,
+        direction_to_delta(Board, From, Dir, To, Final),
+        write('Destination position is valid.').
+        
+  
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow < FromRow, ToCol is FromCol -> Dir = u.
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow > FromRow, ToCol is FromCol -> Dir = d.
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow is FromRow, ToCol < FromCol -> Dir = l.
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow is FromRow, ToCol > FromCol -> Dir = r.
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow < FromRow, ToCol < FromCol -> Dir = ul.
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow < FromRow, ToCol > FromCol -> Dir = ur.
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow > FromRow, ToCol < FromCol -> Dir = dl.
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+        ToRow > FromRow, ToCol > FromCol -> Dir = dr.
+        
+calculate_dir((FromRow, FromCol), (ToRow, ToCol), Dir) :-
+    DiffRow is ToRow - FromRow,
+    DiffCol is ToCol - FromCol,
+    abs(DiffRow) =:= abs(DiffCol),  % Check if it's a perfect diagonal
+    TempRow is FromRow, TempCol is FromCol,
+    checkDiagonalPath(FromRow, FromCol, ToRow, ToCol, Board, e).
 
-% Announce winner (placeholder)
-announce_winner(Board) :-
-    write('Game Over!'),
-    nl.
-    % Generate random board of given size
-    generate_board(Size, Board) :-
-        length(Board, Size),
-        maplist(generate_row(Size), Board).
+checkDiagonalPath(Row, Col, ToRow, ToCol, Board, _) :-
+    Row =:= ToRow, Col =:= ToCol.
+checkDiagonalPath(Row, Col, ToRow, ToCol, Board, Empty) :-
+    NextRow is Row + sign(ToRow - Row),
+    NextCol is Col + sign(ToCol - Col),
+    piece_at(Board, (NextRow, NextCol), Empty),
+    checkDiagonalPath(NextRow, NextCol, ToRow, ToCol, Board, Empty).
+% Direction to delta conversion
 
-    generate_row(Size, Row) :-
-        length(Row, Size),
-        maplist(random_cell, Row).
 
-    random_cell(Cell) :-
-        random_between(1, 2, Cell).
+     OtherPiece is e,
+    Final = (ToRow, NewCol).
 
-    % Initialize board with given size
-    initial_board(Board) :-
-        board_size(Size),
-        generate_board(Size, Board).
+direction_to_delta(Board, (FromRow,FromCol), r,(ToRow, ToCol), Final) :-
+    NewCol is ToCol - 1,
+    valid_position(ToRow, NewCol),
+    piece_at(Board, (ToRow, NewCol), OtherPiece),
+    OtherPiece = e,
+    Final = (ToRow, NewCol).
+direction_to_delta(Board, (FromRow,FromCol), l,(ToRow, ToCol), Final) :-
+    NewCol is ToCol + 1,
+    valid_position(ToRow, NewCol),
+    piece_at(Board, (ToRow, NewCol), OtherPiece),
+    OtherPiece = e,
+    Final = (ToRow, NewCol).
+direction_to_delta(Board, (FromRow,FromCol), u,(ToRow, ToCol), Final) :-
+    NewRow is ToRow + 1,
+    valid_position(NewRow, ToCol),
+    piece_at(Board, (NewRow, ToCol), OtherPiece),
+    OtherPiece = e,
+    Final = (NewRow, ToCol).
+direction_to_delta(Board, (FromRow,FromCol), d,(ToRow, ToCol), Final) :-
+    NewRow is ToRow - 1,
+    valid_position(NewRow, ToCol),
+    piece_at(Board, (NewRow, ToCol), OtherPiece),
+    OtherPiece = e,
+    Final = (NewRow, ToCol).
+direction_to_delta(Board, (FromRow,FromCol), ul,(ToRow, ToCol), Final) :-
+    NewRow is ToRow + 1,
+    NewCol is ToCol + 1,
+    valid_position(NewRow, NewCol),
+    piece_at(Board, (NewRow, NewCol), OtherPiece),
+    OtherPiece = e,
+    Final = (NewRow, NewCol).
+
+direction_to_delta(Board, (FromRow,FromCol), ur,(ToRow, ToCol), Final) :-
+    NewRow is ToRow + 1,
+    NewCol is ToCol - 1,
+    valid_position(NewRow, NewCol),
+    piece_at(Board, (NewRow, NewCol), OtherPiece),
+    OtherPiece = e,
+    Final = (NewRow, NewCol).
+
+direction_to_delta(Board, (FromRow,FromCol), dl,(ToRow, ToCol), Final) :-
+    NewRow is ToRow - 1,
+    NewCol is ToCol + 1,
+    valid_position(NewRow, NewCol),
+    piece_at(Board, (NewRow, NewCol), OtherPiece),
+    OtherPiece = e,
+    Final = (NewRow, NewCol).
+
+direction_to_delta(Board, (FromRow,FromCol), dr,(ToRow, ToCol), Final) :-
+    NewRow is ToRow - 1,
+    NewCol is ToCol - 1,
+    valid_position(NewRow, NewCol),
+    piece_at(Board, (NewRow, NewCol), OtherPiece),
+    OtherPiece = e,
+    Final = (NewRow, NewCol).
+
+    
+direction_to_target(Board, From, Dir, To) :-
+    direction_to_delta(Dir, From, InitialTarget),
+    other_player(Player, OpponentColor),
+    find_collision(Board, From, InitialTarget, To, OpponentColor).
+% Find collision point
+find_collision(Board, (Row, Col), (DRow, DCol), (FinalRow, FinalCol), OpponentColor) :-
+    NextRow is Row + DRow,
+    NextCol is Col + DCol,
+    valid_position(NextRow, NextCol),
+    (piece_at(Board, (NextRow, NextCol), OpponentColor) ->
+        FinalRow = NextRow,
+        FinalCol = NextCol;   
+        piece_at(Board, (NextRow, NextCol), e),
+        find_collision(Board, (NextRow, NextCol), (DRow, DCol), (FinalRow, FinalCol), OpponentColor)
+    ).
+
+% Make a move
+make_move(Board, (FromRow, FromCol),(ToRow, ToCol),(FinalRow,FinalCol), NewBoard) :-
+    piece_at(Board, (FromRow, FromCol), Piece),
+    set_cell(Board, (FromRow, FromCol), e, TempBoard),
+    set_cell(TempBoard, (ToRow, ToCol), e, TempBoard_),
+    set_cell(TempBoard_, (FinalRow, FinalCol), Piece, NewBoard).
+    
+
+% Helper predicates
+piece_at(Board, (Row, Col), Piece) :-
+    nth1(Row, Board, BoardRow),
+    nth1(Col, BoardRow, Piece),
+    write('Piece at source position is correct.'), nl.
+
+set_cell(Board, (Row, Col), Value, NewBoard) :-
+    nth1(Row, Board, OldRow),
+    replace_nth(Col, OldRow, Value, NewRow),
+    replace_nth(Row, Board, NewRow, NewBoard).
+
+replace_nth(1, [_|T], X, [X|T]) :- !.
+replace_nth(N, [H|T], X, [H|R]) :-
+    N > 1,
+    N1 is N - 1,
+    replace_nth(N1, T, X, R).
+
+valid_position(Row, Col) :-
+    board_size(MaxCol, MaxRow),
+    Row > 0, Row =< MaxRow,
+    Col > 0, Col =< MaxCol.
+
+next_player(w, b).
+next_player(b, w).
+
+other_player(w, b).
+other_player(b, w).
+
+% Check if game is over
+game_over(Board, Player) :-
+    \+ can_capture(Board, Player).
+
+% Check if player can capture
+can_capture(Board, Player) :-
+    piece_at(Board, (Row, Col), Player),
+    member(Dir, [u,d,l,r,ul,ur,dl,dr]),
+    valid_move(Board, Player, (Row, Col), Dir, _).
+
+% Announce winner
+announce_winner(Player) :-
+    other_player(Player, Winner),
+    format('Game Over! ~w wins!~n', [Winner]).
+
+play_game(Board, Player) :-
+    format('~w\'s turn.~n', [Player]),
+    get_valid_move(Board, Player, From, To, Final),
+    make_move(Board, From, To, Final, NewBoard),
+    clear_console,
+    display_game(NewBoard),
+    next_player(Player, NextPlayer),
+    play_game(NewBoard, NextPlayer).
--- menu.pl
+++ menu.pl
@@ -1,6 +1,7 @@
+:- consult('game.pl').
+
 % Menu display and handling
 :- dynamic player_mode/1.
-:- dynamic board_size/1.
 
 % Main menu
 display_main_menu :-
@@ -26,24 +27,21 @@ menu :-
 handle_menu_option(1) :-
     retractall(player_mode(_)),
     asserta(player_mode(pvp)),
-    select_board_size,
     initial_board(Board),
     display_game(Board),
-    play_game(Board).
+    play_game(Board, w).
 handle_menu_option(2) :-
     retractall(player_mode(_)),
     asserta(player_mode(pvc)),
-    select_board_size,
     initial_board(Board),
     display_game(Board),
-    play_game(Board).
+    play_game(Board, w).
 handle_menu_option(3) :-
     retractall(player_mode(_)),
     asserta(player_mode(cvc)),
-    select_board_size,
     initial_board(Board),
     display_game(Board),
-    play_game(Board).
+    play_game(Board, w).
 handle_menu_option(4) :-
     display_rules,
     menu.
@@ -55,31 +53,7 @@ handle_menu_option(_) :-
     sleep(2),
     menu.
 
-% Board size selection
-select_board_size :-
-    clear_console,
-    write('Select board size:\n'),
-    write('1. 6x6\n'),
-    write('2. 8x8\n'),
-    write('3. 10x10\n'),
-    read(Size),
-    handle_size_selection(Size).
-
-handle_size_selection(1) :-
-    asserta(board_size(6)),
-    start_game.
-handle_size_selection(2) :-
-    asserta(board_size(8)),
-    start_game.
-handle_size_selection(3) :-
-    asserta(board_size(10)),
-    start_game.
-handle_size_selection(_) :-
-    write('Invalid size! Please try again.\n'),
-    sleep(2),
-    select_board_size.
-
-% Clear console (works for both Unix and Windows)
+% Clear console
 clear_console :-
     write('\e[H\e[2J').
 
@@ -89,15 +63,9 @@ display_rules :-
     write('=================================\n'),
     write('           HOW TO PLAY           \n'),
     write('=================================\n'),
-    write('1. Players take turns selecting cells\n'),
-    write('2. Selected cells collapse and merge with adjacent same-colored cells\n'),
-    write('3. Cells above fall down to fill empty spaces\n'),
-    write('4. Game ends when no more moves are possible\n'),
-    write('5. Player with the highest score wins!\n'),
+    write('1. Players take turns moving their pieces\n'),
+    write('2. Pieces can move in any direction (horizontal, vertical, diagonal)\n'),
+    write('3. A piece must land next to an empty space\n'),
+    write('4. Game ends when a player cannot make any more moves\n'),
     write('\nPress Enter to return to main menu\n'),
     read(_).
-
-% Placeholder for game start
-start_game :-
-    % Add your game initialization here
-    write('Starting game...\n').
